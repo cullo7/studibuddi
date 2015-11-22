7c1c9261ba514a4fe5625957a42de0a6b2699a01
Aiden Cullo <culloaiden3@gmail.com>
2015-11-22 03:47:31 -0500

    created sql database

---
+++ data_handler.js
@@ -0,0 +1,1 @@
+//Has to send information to and get information from database
---
+++ pages/sqlfun.sql
@@ -0,0 +1,1 @@
+UPDATE attacks SET desc="My dad only gave me a small loan of a million\ndollars" WHERE desc="My dad only gave me a small loan of a million dollars";
