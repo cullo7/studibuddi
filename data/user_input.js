6e38680f49235b8c5920b310a1cfa8a93bf7db7e
Aiden Cullo <culloaiden3@gmail.com>
2015-12-01 17:28:01 -0500

    trying to link js to loginInfo.html

--- data/user_input.js
+++ data/user_input.js
@@ -1,4 +1,7 @@
 // purpose of this file will to be take in user input and 
 // call python data_handler.py to acces and manipulate database
 // not completely sure if this file is necessary
-
+console.log("Hello")
+function login(){
+	console.log("Hello");
+}
