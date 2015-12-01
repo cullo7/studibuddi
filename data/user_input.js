2855a1435822e5258e0906f57da609015999a29f
Aiden Cullo <culloaiden3@gmail.com>
2015-12-01 17:14:15 -0500

    began user intake functionality

---
+++ data/user_input.js
@@ -0,0 +1,4 @@
+// purpose of this file will to be take in user input and 
+// call python data_handler.py to acces and manipulate database
+// not completely sure if this file is necessary
+
--- pages/sqlfun.sql
+++
@@ -1,1 +0,0 @@
-UPDATE attacks SET desc="My dad only gave me a small loan of a million\ndollars" WHERE desc="My dad only gave me a small loan of a million dollars";
