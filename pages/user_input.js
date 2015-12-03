5d07c6f220cdbf8dd0fe307a48107843703516fc
Aiden Cullo <culloaiden3@gmail.com>
2015-12-02 19:31:16 -0500

    still trying to link js to login html

--- pages/user_input.js
+++ pages/user_input.js
@@ -1,7 +1,8 @@
 // purpose of this file will to be take in user input and 
 // call python data_handler.py to acces and manipulate database
 // not completely sure if this file is necessary
-console.log("Hello")
+console.log("Hello");
+alert("Hello");
 function login(){
 	console.log("Hello");
 }
